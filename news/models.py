b6b70b04581152f42e433735db08f054546a1db2
AmirxonEngineer <amirshohxaitov98@gmail.com>
2024-08-03 20:26:48 +0500

    second commit

--- news/models.py
+++ news/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=150)
+    auhtor = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE
+    )
+    body = models.TextField()
 
 
     def __str__(self):
